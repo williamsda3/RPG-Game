e4176794da89adfe1a8896a7be0803647432dfa9
David Williams <williamsda3@vcu.edu>
2023-09-24 18:34:01 -0400

    Fixing matchmaking

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 # from flask_socketio import SocketIO  # Import SocketIO from flask_socketio
@@ -91,7 +92,7 @@ def create_player():
 
 
 
-# Created a function to reduce the players hp as is gets moved to the users Cart
+# Created a function to reduce the players hp 
 @app.route('/players/<int:player_id>/reduce/<int:dmg>', methods=['POST'])
 def reduce_player(player_id, dmg):
     player = PlayerStats.query.get_or_404(player_id)
@@ -176,7 +177,70 @@ def check_invitations(receiver_id):
 
     return jsonify({'invitations': [inv.as_dict() for inv in pending_invitations]}), 200
 
+# WORKING MATCHMAKING
 
+# User database (for demonstration purposes)
+users_db = {
+    'user1': {'password': 'password1', 'id': 1},
+    'user2': {'password': 'password2', 'id': 2}
+}
+
+# Invitation database (for demonstration purposes)
+invitations_db = []
+
+def authenticate_user(username, password):
+    # Check if the username exists in the database
+    if username in users_db:
+        user = users_db[username]
+        # Check if the provided password matches the stored password
+        if user['password'] == password:
+            return user['id']  # Return the user ID as a form of user identification
+
+    return None  # Return None if authentication fails
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Check if the provided credentials are valid (e.g., against a database)
+        # Set the user's ID in the session upon successful login
+        user_id = authenticate_user(request.form['username'], request.form['password'])
+        if user_id:
+            session['user_id'] = user_id
+            return render_template("dash.html")  # Redirect to a protected route
+        else:
+            return 'Invalid credentials. Please try again.'
+
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    # Check if the user is authenticated (i.e., user ID is in the session)
+    if 'user_id' in session:
+        # Load the dashboard for the authenticated user
+        return 'Welcome to the dashboard, user {}'.format(session['user_id'])
+    else:
+        return redirect(url_for('login'))  # Redirect to login if not authenticated
+
+@app.route('/send_invite', methods=['POST'])
+def send_invite():
+    if 'user_id' in session:
+        sender_id = session['user_id']
+        receiver_id = request.form['receiver_id']  # Assuming a form with 'receiver_id'
+        # Create an invitation and add it to the invitation database
+        invitation = {'sender_id': sender_id, 'receiver_id': receiver_id}
+        invitations_db.append(invitation)
+        return 'Invitation sent successfully.'
+    else:
+        return 'User not authenticated.'
+
+@app.route('/view_invites')
+def view_invites():
+    if 'user_id' in session:
+        user_id = session['user_id']
+        received_invites = [invite for invite in invitations_db if invite['receiver_id'] == user_id]
+        return jsonify({'received_invites': received_invites})
+    else:
+        return 'User not authenticated.'
 
 
 
